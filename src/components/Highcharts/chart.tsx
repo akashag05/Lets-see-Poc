b2674b79851f7bce5f71295d94c03aeea6ef0637
akashag05 <124151772+akashag05@users.noreply.github.com>
2023-07-28 15:01:57 +0530

    chart visualization , chart table

--- src/components/Highcharts/chart.tsx
+++ src/components/Highcharts/chart.tsx
@@ -1,53 +1,139 @@
-import React from "react";
-import Highcharts from "highcharts/highstock";
-import HighchartsReact from "highcharts-react-official";
+import React, { useEffect } from "react";
+import Highcharts from "highcharts";
+import HighchartsExporting from "highcharts/modules/exporting";
+import HighchartsExportData from "highcharts/modules/export-data";
+import HighchartsAccessibility from "highcharts/modules/accessibility";
+import { ChartTable } from "../Table/ChartTable";
 
-const chart = () => {
-  const data = [
-    [1689865857, 30.72], // [timestamp, value]
-    [1689952257, 34.85],
-    [1690038657, 45.89],
-    [1690125057, 88.95],
-    [1690211457, 87.105],
-    [1690297857, 67.123],
-    [1690387857, 76.678],
-    [1690474257, 65.786],
-    [1690517457, 88.975],
-    // [1538951402876, 71.23],
-    // [1538951402976, 71.87],
-    // Add more data points as needed
-  ];
+HighchartsExporting(Highcharts);
+HighchartsExportData(Highcharts);
+HighchartsAccessibility(Highcharts);
 
-  const options = {
-    chart: {
-      zoomType: "x",
-    },
-    title: {
-      text: "Zoomable Time Series Chart",
-    },
-    xAxis: {
-      type: "datetime",
-    },
-    series: [
-      {
-        name: "Dummy Data",
-        data: data,
-        tooltip: {
-          valueDecimals: 2,
-        },
-      },
-    ],
-  };
+const Chart: React.FC = () => {
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await fetch(
+          "https://cdn.jsdelivr.net/gh/highcharts/highcharts@v10.3.3/samples/data/usdeur.json"
+        );
+        const data = await response.json();
+
+        Highcharts.chart("container", {
+          chart: {
+            zoomType: "x",
+            events: {
+              load: function () {
+                const chart = this;
+                const logoWidth = 20;
+                const logoHeight = 20;
+                const logoX = chart.plotLeft - 60;
+                const logoY = chart.plotTop - 60;
+
+                // chart.renderer
+                //   .text(
+                //     "RevDau Industries Pvt. Ltd",
+                //     logoX + logoWidth + 10,
+                //     logoY + 15
+                //   )
+                //   .css({
+                //     color: "#666",
+                //     fontSize: "14px",
+                //     fontWeight: "bold",
+                //   })
+                //   .add();
+                // chart.renderer
+                //   .text(
+                //     "Created By RevDau Industries Pvt. Ltd.",
+                //     250,
+                //     chart.plotTop + chart.plotHeight + 40
+                //   )
+                //   .css({
+                //     color: "#666",
+                //     fontSize: "12px",
+                //   })
+                //   .add();
+              },
+            },
+          },
+          title: {
+            text: "USD to EUR exchange rate over time",
+            align: "left",
+            y: 40,
+          },
+          subtitle: {
+            text:
+              document.ontouchstart === undefined
+                ? "Click and drag in the plot area to zoom in"
+                : "Pinch the chart to zoom in",
+            align: "left",
+            y: 60,
+          },
+          xAxis: {
+            type: "datetime",
+          },
+          yAxis: {
+            title: {
+              text: "Exchange rate",
+            },
+          },
+          legend: {
+            enabled: false,
+          },
+          plotOptions: {
+            area: {
+              fillColor: {
+                linearGradient: {
+                  x1: 0,
+                  y1: 0,
+                  x2: 0,
+                  y2: 1,
+                },
+                stops: [
+                  [0, Highcharts.getOptions().colors[0]],
+                  [
+                    1,
+                    Highcharts.color(Highcharts.getOptions().colors[0])
+                      .setOpacity(0)
+                      .get("rgba"),
+                  ],
+                ],
+              },
+              marker: {
+                radius: 2,
+              },
+              lineWidth: 1,
+              states: {
+                hover: {
+                  lineWidth: 1,
+                },
+              },
+              threshold: null,
+            },
+          },
+          series: [
+            {
+              type: "area",
+              name: "USD to EUR",
+              data: data,
+            },
+          ],
+        });
+      } catch (error) {
+        console.error("Error fetching chart data:", error);
+      }
+    };
+
+    fetchChartData();
+  }, []);
 
   return (
     <div>
-      <HighchartsReact
-        highcharts={Highcharts}
-        constructorType={"stockChart"}
-        options={options}
-      />
+      <figure className="highcharts-figure my-4">
+        <div id="container" />
+      </figure>
+      <ChartTable />
     </div>
   );
 };
 
-export default chart;
+export default Chart;
